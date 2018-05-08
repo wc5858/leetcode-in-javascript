3cb0c0c2ce7b33acda7f5a3f4df92f469f63f37d
ash <475015191@qq.com>
2018-05-08 09:36:54 +0800

    answer 1&2

---
+++ 1~20/1-TwoSum.js
@@ -0,0 +1,28 @@
+/**
+ * Question:
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * 
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let hash = {}
+  for (let i = 0; i < nums.length; i++) {
+    let key = nums[i]
+    let data = hash[target - key]
+    if (data !== undefined) {
+      return [data, i]
+    }
+    hash[key] = i
+  }
+}
---
+++ 1~20/2-AddTwoNumbers.js
@@ -0,0 +1,47 @@
+/**
+ * Question:
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  let res
+  let cur
+  let append = 0
+  do {
+    let val = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + append
+    if (res) {
+      cur.next = new ListNode(val % 10)
+      cur = cur.next
+    } else {
+      res = new ListNode(val % 10)
+      cur = res
+    }
+    append = Math.floor(val / 10)
+    l1 = l1 ? l1.next : null
+    l2 = l2 ? l2.next : null
+  } while (l1 != null || l2 != null)
+  if (append != 0) {
+    cur.next = new ListNode(append)
+  }
+  return res
+}
