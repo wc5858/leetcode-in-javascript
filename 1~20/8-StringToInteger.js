7df2d791a8945367440e6f276876e057694d226c
ash <475015191@qq.com>
2018-05-08 09:46:40 +0800

    answer 8

---
+++ 1~20/8-StringToInteger.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+  let res = parseInt(str.trim()) || 0
+  if (res > 2147483647) res = 2147483647
+  if (res < -2147483648) res = -2147483648
+  return res
+};
