77b0669b3b13411985bb2659c1893992a5c70743
ash <475015191@qq.com>
2018-05-15 15:41:28 +0800

    answer11&13;rename

---
+++ 1~20/11.ContainerWithMostWater.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let left = 0
+  let right = height.length - 1
+  let max = 0
+  while (left < right) {
+    max = Math.max(max, (right - left) * Math.min(height[left], height[right]))
+    if (height[right] > height[left]) {
+      left++
+    } else {
+      right--
+    }
+  }
+  return max
+};
---
+++ 1~20/13.RomanToInteger.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let nums = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+  let last = 0
+  let res = 0
+  let cur = 0
+  for (let i = s.length - 1; i >= 0; i--) {
+    cur = nums[s[i]]
+    if (cur <= last) {
+      res -= cur
+    } else {
+      res += cur
+    }
+    last = cur
+  }
+  return res
+};
+
+console.log(romanToInt("MCMXCIV"))
