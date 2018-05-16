4139dc08912fe848982149a27e4ff4f87219f785
ash <475015191@qq.com>
2018-05-16 10:05:03 +0800

    answer14

---
+++ 1~20/14.LongestCommonPrefix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  let result = ''
+  let last
+  if(strs.length === 0) {
+    return ''
+  }
+  if(strs.length === 1) {
+    return strs[0]
+  }
+  outer:for(let cursor = 0;true;cursor++) {
+    last = ''
+    for(let i = 0;i<strs.length;i++) {
+      if(cursor>=strs[i].length || (last && strs[i][cursor] !== last)) {
+        break outer
+      } else if(last && strs[i][cursor] === last && i===strs.length - 1) {
+        result += last
+      } else if(!last) {
+        last = strs[i][cursor]
+      }
+    }
+  }
+  return result
+};
