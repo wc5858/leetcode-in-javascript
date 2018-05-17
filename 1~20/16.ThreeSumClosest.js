ad435fdd75afda69e5c9293d1ce45f4f3ec04ffe
ash <475015191@qq.com>
2018-05-17 14:18:26 +0800

    answer16&17

---
+++ 1~20/16.ThreeSumClosest.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  if (nums.length < 3) return false
+  // sort first
+  let diff = Infinity
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < nums.length - 2; i++) {
+    // Never let i refer to the same value twice to avoid duplicates.
+    if (i != 0 && nums[i] == nums[i - 1]) continue
+    let j = i + 1
+    let k = nums.length - 1
+    let last = Infinity
+    while (j < k) {
+      let d = nums[i] + nums[j] + nums[k] - target
+      if (d === 0) {
+        return target
+      }
+      if (Math.abs(d) <= Math.abs(last)) {
+        if (d < 0) {
+          j++
+        } else {
+          k--
+        }
+        last = d
+      } else {
+        if (d > 0 && last < 0) {
+          k--
+        } else if (d < 0 && last > 0) {
+          j++
+        } else {
+          break
+        }
+      }
+    }
+    if (Math.abs(last) < Math.abs(diff)) {
+      diff = last
+    }
+  }
+  return target + diff
+}
+
+console.log(
+  threeSumClosest([-55, -24, -18, -11, -7, -3, 4, 5, 6, 9, 11, 23, 33], 0),
+)
---
+++ 1~20/17.LetterCombinationsOfAPhoneNumber.js
@@ -0,0 +1,56 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  let result = []
+  let temp = []
+  for (let i = 0; i < digits.length; i++) {
+    if (result.length === 0) {
+      let code = 97 + (+digits[i] - 2) * 3
+      switch (digits[i]) {
+        case '7':
+          result.push('s')
+          break
+        case '8':
+          code++
+          break
+        case '9':
+          code++
+          result.push('z')
+          break
+        default:
+          break
+      }
+      for (let j = 0; j < 3; j++) {
+        result.push(String.fromCharCode(code + j))
+      }
+    } else {
+      result.forEach(val => {
+        let code = 97 + (+digits[i] - 2) * 3
+        switch (digits[i]) {
+          case '7':
+            temp.push(val + 's')
+            break
+          case '8':
+            code++
+            break
+          case '9':
+            code++
+            temp.push(val + 'z')
+            break
+          default:
+            break
+        }
+        for (let j = 0; j < 3; j++) {
+          temp.push(val + String.fromCharCode(code + j))
+        }
+      })
+      result = temp
+      temp = []
+    }
+  }
+  return result
+}
+
+console.log(letterCombinations('999'))
