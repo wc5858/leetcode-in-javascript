78dcab776db3316ba6c544840e210b32c54ef53b
ash <475015191@qq.com>
2018-05-25 15:23:48 +0800

    25

---
+++ 21~40/26.RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if(nums.length === 0) return 0
+  if(nums.length === 1) return 1
+  let index = 1
+  for(let i = 1; i<nums.length;i++) {
+    if(nums[i]!==nums[i-1]) {
+      nums[index] = nums[i]
+      index++
+    }
+  }
+  return index
+};
