a756a70c699344762af2cfa808c466e049b28785
ash <475015191@qq.com>
2018-05-28 10:48:46 +0800

    66

---
+++ 61~80/66.PlusOne.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  let index = digits.length - 1
+  while (digits[index] == 9) {
+    digits[index] = 0
+    index--
+  }
+  if (index >= 0) {
+    digits[index]++
+  } else {
+    digits.unshift(1)
+  }
+  return digits
+}
