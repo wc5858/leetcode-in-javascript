fcb239a29ff16e4db0420bc799070b9099b1f656
ash <475015191@qq.com>
2018-05-28 10:16:06 +0800

    27

---
+++ 21~40/27.RemoveElement.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  let len = nums.length
+  for (let i = 0; i < len; i++) {
+    if (nums[i] === val) {
+      while (nums[len - 1] === val) {
+        len--
+      }
+      nums[i] = nums[len - 1]
+      if (i < len - 1) {
+        len--
+      }
+    }
+  }
+  return len
+}
