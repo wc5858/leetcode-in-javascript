b5197ccdd70fc44fa64f5f4046a22b2f276a6adb
ash <475015191@qq.com>
2018-05-29 14:53:28 +0800

    32

---
+++ 21~40/32.LongestValidParentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// 视图用栈做，思路有偏差，没有解决取最大值的问题
+// 以下是看的solution。同样是用栈，但是把index推入栈记录了位置信息，颇为巧妙
+var longestValidParentheses = function(s) {
+  let maxans = 0
+  const stack = [-1]
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '(') {
+      stack.push(i)
+    } else {
+      stack.pop()
+      if (stack.length === 0) {
+        stack.push(i)
+      } else {
+        maxans = Math.max(maxans, i - stack[stack.length-1])
+      }
+    }
+  }
+  return maxans
+}
+console.log(longestValidParentheses('(()'))
