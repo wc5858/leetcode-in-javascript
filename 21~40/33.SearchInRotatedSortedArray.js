3f6eb9ffe481f31b57cc8aea3f7cba02d5c12faa
ash <475015191@qq.com>
2018-05-30 13:42:24 +0800

    33

--- 21~40/33.SearchInRotatedSortedArray.js
+++ 21~40/33.SearchInRotatedSortedArray.js
@@ -12,8 +12,13 @@ var search = function(nums, target) {
  * @return {number}
  */
 var biSearch = function(nums, start, end, target) {
+  
   if (start === end - 1) {
     return nums[end] === target ? end : nums[start] === target ? start : -1
+  }
+  if(nums[end] > nums[start] &&(target > nums[end]||target < nums[start])) {
+    // 这个判断非必要，可以在某些情况下提前终止二叉查找，但是会引入额外的判断
+    return -1
   }
   let mid = middle(start, end)
   if (nums[start] < nums[mid]) {
