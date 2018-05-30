247ac3dfe5816bea7cd2f08cf249fd7e2bd22af8
ash <475015191@qq.com>
2018-05-30 16:47:48 +0800

    35

--- 21~40/34.SearchForARange.js
+++ 21~40/34.SearchForARange.js
@@ -11,9 +11,6 @@
 // 翻了翻v8源码，发现indexOf已经在16年的时候从js实现迁移到c++实现了- -看不太懂
 // 相关commit地址：https://github.com/v8/v8/commit/da5d713d73d43aec66ff45c29d06b4857182578b#diff-500b17e3a1341a99917d087572ab8163
 // 所以还是不能偷懒
-var middle = function(i, j) {
-  return i + Math.floor((j - i) / 2)
-}
 var searchRange = function(nums, target) {
   if (nums.length === 0) return [-1, -1]
   if (nums.length === 1) return nums[0] === target ? [0, 0] : [-1, -1]
@@ -24,6 +21,9 @@ var searchRange = function(nums, target) {
   let i = left === 0 ? 0 : left - 1
   let right = biSearch(nums, i, nums.length - 1, target, false)
   return left < right ? [left, right] : [right, left]
+}
+var middle = function(i, j) {
+  return i + Math.floor((j - i) / 2)
 }
 var biSearch = function(nums, start, end, target, direction) {
   // direction为true代表从左往右查找
