eda77b3807c830c57e7e3bf4f436e157640df086
ash <475015191@qq.com>
2018-05-31 15:48:32 +0800

    38

---
+++ 21~40/38.CountAndSay.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+  let str = '1'
+  for(let i=1;i<n;i++) {
+    let temp = ''
+    let count = 0
+    let pre = undefined
+    for(let j=0;j<str.length;j++) {
+      if(str[j] === pre || pre === undefined) {
+        count++
+      } else {
+        temp += count + pre
+        count = 1
+      }
+      pre = str[j]
+    }
+    temp += count + str[str.length-1]
+    str = temp
+  }
+  return str
+};
