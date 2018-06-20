eebd7f3f34b40bc936d89fc3c98578e9d74f9fb5
ash <wc47501@gmail.com>
2018-06-20 17:30:15 +0800

    ans50

---
+++ 41~60/50.Pow.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  let flag = n < 0
+  let res = 1
+  n = Math.abs(n)
+  while (n) {
+    if (n % 2) {
+      res *= x
+    }
+    x *= x
+    n = Math.floor(n / 2)
+  }
+  return flag ? 1 / res : res
+}
+console.log(myPow(2, -5))
+console.log(Math.pow(2, -5))
