b3cdb8eddb9daa35394d765ba950e7e7df934767
ash <wc47501@gmail.com>
2018-06-22 09:57:44 +0800

    ans55

---
+++ 41~60/55.JumpGame.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let next = 0
+  for (let i = 0; i < nums.length - 1; i++) {
+    // 标记后面最远能到的点
+    if (next < i) return false
+    next = Math.max(nums[i] + i, next)
+  }
+  return next >= nums.length - 1
+};
+console.log(canJump([2,3,1,1,4]))
