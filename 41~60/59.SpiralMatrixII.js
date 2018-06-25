e2d82f129a51acca7b2270acda9faa27dba48260
ash <wc47501@gmail.com>
2018-06-25 18:41:38 +0800

    ans59

---
+++ 41~60/59.SpiralMatrixII.js
@@ -0,0 +1,45 @@
+var generateMatrix = function(n) {
+  let matrix = [],
+    i = 0,
+    j = 0,
+    dir = 0,
+    limit = [0, 0, n - 1, n - 1],
+    k = n * n
+  for (var m = 1; m <= k; m++) {
+    if (!matrix[i]) {
+      matrix[i] = new Array(n)
+    }
+    matrix[i][j] = m
+    switch (dir) {
+      case 0:
+        j++
+        if (j === limit[2]) {
+          dir = 1
+          limit[0]++
+        }
+        break
+      case 1:
+        i++
+        if (i === limit[3]) {
+          dir = 2
+          limit[2]--
+        }
+        break
+      case 2:
+        j--
+        if (j === limit[1]) {
+          dir = 3
+          limit[3]--
+        }
+        break
+      case 3:
+        i--
+        if (i === limit[0]) {
+          dir = 0
+          limit[1]++
+        }
+        break
+    }
+  }
+  return matrix
+}
