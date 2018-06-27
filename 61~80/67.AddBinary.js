61504ed579e66748a5699b62949e099893f8c704
ash <wc47501@gmail.com>
2018-06-27 14:05:15 +0800

    ans67

---
+++ 61~80/67.AddBinary.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  let m = a.length,
+    n = b.length
+  if (m > n) return addBinary(b, a)
+  let carry = 0
+  let i = 1
+  let res = ''
+  while (i <= a.length) {
+    let sum = carry + +a[a.length - i] + +b[b.length - i]
+    carry = Math.floor(sum / 2)
+    res = (sum % 2) + res
+    i++
+  }
+  while (b.length - i >= 0 && carry != 0) {
+    console.log(res,carry,carry & b[b.length - i])
+    res = (carry ^ b[b.length - i]) + res
+    carry = carry & b[b.length - i]
+    i++
+  }
+  console.log(res)
+  return (carry ? carry : b.substr(0, b.length - i + 1)) + res
+}
+// console.log(addBinary('1010', '1'))
+// console.log(addBinary('1010', '1011'))
+// console.log(addBinary('11', '1'))
+console.log(addBinary('101111', '10'))
