16e377fe89310d778ace3bf3076195de807c7e20
ash <wc47501@gmail.com>
2018-06-29 10:44:25 +0800

    ans80

---
+++ 61~80/80.RemoveDuplicatesFromSortedArrayII.js
@@ -0,0 +1,20 @@
+var removeDuplicates = function(nums) {
+  let len = nums.length
+  if (len <= 2) return len
+  let i = 0
+  let index = 0
+  while (i < nums.length) {
+    let prev = nums[i],
+      duplicatesCount = 0
+    while (nums[++i] === prev) {
+      duplicatesCount++
+    }
+    nums[index++] = prev
+    if (duplicatesCount) {
+      nums[index++] = prev
+    }
+  }
+  return index
+}
+let nums = [1,1,1,2,2,3]
+console.log(removeDuplicates(nums), nums)
