e58e87f580d1d8258a21ad391aea2d5556a2ba1e
ash <wc47501@gmail.com>
2018-06-29 19:02:09 +0800

    edit73

--- 61~80/73.SetMatrixZeroes.js
+++ 61~80/73.SetMatrixZeroes.js
@@ -27,5 +27,24 @@
   setZeroes(data)
   console.log(data)
 }
-// 这里有给更巧妙的解法
+// 这里有给更巧妙的解法：用首行和首列来记录状态
 // https://leetcode.com/problems/set-matrix-zeroes/discuss/26014/Any-shorter-O(1)-space-solution
+{
+  var setZeroes = function(matrix) {
+    let col0 = 1,
+      rows = matrix.length,
+      cols = matrix[0].length
+
+    for (let i = 0; i < rows; i++) {
+      if (matrix[i][0] == 0) col0 = 0
+      for (let j = 1; j < cols; j++)
+        if (matrix[i][j] == 0) matrix[i][0] = matrix[0][j] = 0
+    }
+    // 第二次从后往前遍历，防止记录被修改
+    for (let i = rows - 1; i >= 0; i--) {
+      for (let j = cols - 1; j >= 1; j--)
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0
+      if (col0 == 0) matrix[i][0] = 0
+    }
+  }
+}
