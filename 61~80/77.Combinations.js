a6e00b601692a9ac7e417c4dba541cac3011454c
ash <wc47501@gmail.com>
2018-07-02 17:13:29 +0800

    ans77

---
+++ 61~80/77.Combinations.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+  if(!k) return []
+  let ans = []
+  function cb(pre,cur,rest) {
+    console.log(pre)
+    if(!rest) {
+      ans.push(pre.split('-').map(i=>+i))
+      return
+    }
+    for(let i = cur;i<=n-rest+1;i++) {
+      // 这个循环中 如果把n-rest+1改成n，一样能得到结果，但是性能下降很多
+      cb(pre + (pre?'-':'')+i,i+1,rest-1)
+    }
+  }
+  cb('',1,k)
+  return ans
+};
+console.log(combine(4,3))
