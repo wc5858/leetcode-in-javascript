fdd7abd5b49f4c6c5114a76e78b0b9eb3d169c28
ash <wc47501@gmail.com>
2018-07-05 11:30:28 +0800

    ans90

---
+++ 81~100/90.SubsetsII.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  let set = new Set()
+  let n = nums.length
+  nums.sort((a, b) => a - b)
+  function cb(pre, cur) {
+    if (pre) {
+      set.add(pre)
+    }
+    for (let i = cur; i < n; i++) {
+      cb(pre + (pre ? '_' : '') + nums[i], i + 1)
+    }
+  }
+  cb('', 0)
+  let ans = [[]]
+  for (let i of set.values()) {
+    ans.push(i.split('_').map(i => +i))
+  }
+  return ans
+};
+console.log(subsetsWithDup([4, 4, 4, 1, 4]))
