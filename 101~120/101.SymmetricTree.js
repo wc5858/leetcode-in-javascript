563cc578ae207b6a0c2e8a0d731c07dad3d0ad10
ash <wc47501@gmail.com>
2018-07-12 16:49:56 +0800

    ans101

---
+++ 101~120/101.SymmetricTree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  return root === null || (function isSymmetric(left, right) {
+    return (left === null && right === null) ||
+      (left!== null && right!== null && left.val === right.val &&
+        isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left))
+  })(root.left, root.right)
+};
