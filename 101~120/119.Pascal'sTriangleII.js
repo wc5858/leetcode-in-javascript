87e47edd07e7eb241bebe2791fd8baad5a5dcae1
ash <wc47501@gmail.com>
2018-07-13 16:08:37 +0800

    ans119

---
+++ 101~120/119.Pascal'sTriangleII.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+// var getRow = function (rowIndex) {
+//   let ans = [1]
+//   for (let i = 1; i <= rowIndex; i++) {
+//     ans.unshift(1)
+//     for (let j = 1; j < i; j++) {
+//       ans[j] += ans[j + 1]
+//     }
+//   }
+//   return ans
+// };
+// 用push从后往前操作比用unshift好很多，想必极有可能是因为unshift移动数组的开销
+var getRow = function (rowIndex) {
+  let ans = [1]
+  for (let i = 1; i <= rowIndex; i++) {
+    ans.push(1)
+    for (let j = ans.length - 2; j > 0; j--) {
+      ans[j] += ans[j - 1]
+    }
+  }
+  return ans
+};
+console.log(getRow(2))
+console.log(getRow(3))
+console.log(getRow(4))
+console.log(getRow(5))
