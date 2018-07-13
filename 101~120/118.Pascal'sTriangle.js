e2d2f13d4445faf6f285d3da9aff03d664fe0516
ash <wc47501@gmail.com>
2018-07-13 14:11:46 +0800

    ans118

---
+++ 101~120/118.Pascal'sTriangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  let ans = []
+  for(let i = 0;i<numRows;i++) {
+    let row = [1]
+    for(let j = 1;j < i;j++) {
+      row[j] = ans[i-1][j-1] + ans[i-1][j]
+    }
+    if(i>0) {
+      row.push(1)
+    }
+    ans.push(row)
+  }
+  return ans
+};
+console.log(generate(1))
+console.log(generate(2))
+console.log(generate(5))
