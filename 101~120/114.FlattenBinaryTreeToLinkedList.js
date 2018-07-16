11eae2e2ae0f8957c6904959d3b7d2c8555dec61
ash <wc47501@gmail.com>
2018-07-16 18:30:39 +0800

    ans114

---
+++ 101~120/114.FlattenBinaryTreeToLinkedList.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+  if(root === null) return
+  function helper(root) {
+    let leftIsNull = root.left === null
+    let rightIsNull = root.right === null
+    if(leftIsNull && rightIsNull) {
+      return root
+    }
+    if(leftIsNull && !rightIsNull) {
+      return helper(root.right)
+    }
+    if(!leftIsNull && rightIsNull) {
+      root.right = root.left
+      root.left = null
+      return helper(root.right)
+    }
+    let temp = helper(root.right)
+    helper(root.left).right = root.right
+    root.right = root.left
+    root.left = null
+    return temp
+  }
+  helper(root)
+};
