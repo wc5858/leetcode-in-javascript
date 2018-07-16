44a60edbec90de20c8f1e5aa547b938fbdd1feed
ash <wc47501@gmail.com>
2018-07-16 17:14:44 +0800

    ans110

---
+++ 101~120/110.BalancedBinaryTree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  return false !== (function helper(node) {
+    // helper在非平衡的情况下返回false，否则返回树的高度
+    if (node === null) return 0
+    let left = helper(node.left)
+    let right = helper(node.right)
+    if (left === false && right === false || Math.abs(left - right) > 1) {
+      return false
+    }
+    return Math.max(left, right) + 1
+  })(root)
+};
