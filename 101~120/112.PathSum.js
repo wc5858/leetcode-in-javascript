201d0ee4953cac529144fc0f52064af391c4d361
ash <wc47501@gmail.com>
2018-07-16 17:59:00 +0800

    ans113

--- 101~120/112.PathSum.js
+++ 101~120/112.PathSum.js
@@ -12,7 +12,7 @@
  */
 var hasPathSum = function (root, sum) {
   if (root === null) return false
-  if (root.left === null && root.right === null) return root === sum
+  if (root.left === null && root.right === null) return root.val === sum
   if (root.left === null) return hasPathSum(root.right, sum - root.val)
   if (root.right === null) return hasPathSum(root.left, sum - root.val)
   return hasPathSum(root.right, sum - root.val) || hasPathSum(root.left, sum - root.val)
---
+++ 101~120/113.PathSumII.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function (root, sum) {
+  let ans = []
+  let path = [];
+  (function helper(root, sum) {
+    if (root === null) return
+    if (root.left === null && root.right === null) {
+      if (root.val === sum) {
+        ans.push([...path, sum])
+      }
+      return
+    }
+    path.push(root.val)
+    if (root.right !== null) {
+      helper(root.right, sum - root.val)
+    }
+    if (root.left !== null) {
+      helper(root.left, sum - root.val)
+    }
+    path.pop()
+  })(root, sum)
+  return ans
+};
