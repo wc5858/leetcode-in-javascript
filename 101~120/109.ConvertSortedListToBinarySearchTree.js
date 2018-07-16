56c2a328b6a94fea296a9b87df45cc62fc5d6fa7
ash <wc47501@gmail.com>
2018-07-16 16:44:22 +0800

    ans109

---
+++ 101~120/109.ConvertSortedListToBinarySearchTree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+  // 代价是O(n)的空间复杂度
+  let arr = []
+  while(head) {
+    arr.push(head.val)
+    head = head.next
+  }
+  return sortedArrayToBST(arr)
+};
+var sortedArrayToBST = function (nums) {
+  return (function toTree(left, right) {
+    if (left > right) return null
+    let mid = Math.floor((left + right) / 2)
+    let node = new TreeNode(nums[mid])
+    node.left = toTree(left, mid - 1)
+    node.right = toTree(mid + 1, right)
+    return node
+  })(0, nums.length - 1)
+};
