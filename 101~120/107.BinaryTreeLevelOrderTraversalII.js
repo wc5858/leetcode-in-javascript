13a7ce1085f9f44150d12dfa04fd4c0421baa4d6
ash <wc47501@gmail.com>
2018-07-16 15:24:39 +0800

    ans107

---
+++ 101~120/107.BinaryTreeLevelOrderTraversalII.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  if (root === null) return []
+    let queue = [root]
+    let ans = []
+    while (queue.length > 0) {
+      let len = queue.length
+      let data = []
+      for (let i = 0; i < len; i++) {
+        let cur = queue.shift()
+        if (cur.left!==null) {
+          queue.push(cur.left)
+        }
+        if (cur.right!==null) {
+          queue.push(cur.right)
+        }
+        data.push(cur.val)
+      }
+      ans.unshift(data)
+    }
+    return ans
+};
