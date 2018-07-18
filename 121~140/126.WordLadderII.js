b5c66704f8c45594b4b1eed7d3ee45d1dace6d74
ash <wc47501@gmail.com>
2018-07-18 15:49:16 +0800

    ans126

--- 121~140/126.WordLadderII.js
+++ 121~140/126.WordLadderII.js
@@ -59,3 +59,174 @@
   console.log(findLadders("red", "tax", ["ted", "tex", "red", "tax", "tad", "den", "rex", "pee"]))
   // [["red","ted","tad","tax"],["red","ted","tex","tax"],["red","rex","tex","tax"]]
 }
+{
+  // 其他人的解法，稍微改了下
+  let findLadders = function (start, end, dict) {
+    // Constants and Check functions.
+    var A_CODE = 'a'.charCodeAt(0)
+    var WORD_COUNTS = 26
+    var wordLength = start.length
+    var results = []
+    var dictSet = new Set(dict)
+
+    // Check its from -> to is valid operation.
+    function isValid(from, to) {
+      var i = 0, c = 0
+      while (i < wordLength) {
+        if (from.charCodeAt(i) !== to.charCodeAt(i)) {
+          ++c
+          if (c > 1) break
+        }
+        ++i
+      }
+      return (c === 1)
+    }
+
+    // Create replace str, e.g : 'abc' => 'azc', 'adc' ....etc.
+    var replacedWord = function (word, idx, chCode) {
+      return word.substr(0, idx) + String.fromCharCode(chCode) + word.substr(idx + 1)
+    }
+
+    // If its only one step from start to end.
+    if (isValid(start, end)) {
+      return [[start, end]]
+    }
+    if (!dictSet.has(end)) {
+      return []
+    }
+
+    var startSet = new Set([start])
+    var endSet = new Set([end])
+    var startPath = [[start]]
+    var endPath = [[end]]
+
+    //dict.add(end);
+    var isReversing = false;
+    var isConnected = false;
+
+    // Use to decide whether use all word possible or use all dice word.
+    // 这里作者根据数据量量级选择算法，从而进行优化
+    var wordCombinations = WORD_COUNTS * wordLength;
+    var dictComputations;
+
+    // Determine current paths.
+    var currentPaths;
+    var currentLength;
+    var currentSet;
+    var pathLength;
+
+    // Next path.
+    var nextPaths;
+
+    // Loop vars.
+    var currentPath, currentWord, targets, target, tmpPath;
+
+    // Loop variables.
+    var i, j, k;
+
+    // Init
+    currentPaths = startPath;
+    currentSet = startSet;
+    currentLength = currentPaths.length;
+
+    while (currentLength > 0) {
+      nextPaths = [];
+      // Remove words in current set.
+      targets = currentSet.keys();
+      for (target of targets) {
+        dictSet.delete(target);
+      }
+      currentSet.clear();
+      dictComputations = dictSet.size * wordLength;
+      // Decide whether to use dict iteration of word replaces.
+      if (dictComputations < wordCombinations) {
+        // If iteration though dict needs less compares, iterate it.
+        for (i = 0; i < currentLength; ++i) {
+          currentPath = currentPaths[i];
+          currentWord = currentPath[currentPath.length - 1];
+          targets = dictSet.keys();
+          for (target of targets) {
+            if (isValid(currentWord, target)) {
+              tmpPath = currentPath.slice();
+              tmpPath.push(target);
+              nextPaths.push(tmpPath);
+              currentSet.add(target);
+            }
+          }
+        }
+      } else {
+        // Otherwise, use bruteforce to check all possibilities.
+        for (i = 0; i < currentLength; ++i) {
+          currentPath = currentPaths[i];
+          currentWord = currentPath[currentPath.length - 1];
+          for (j = 0; j < wordLength; ++j) {
+            for (k = 0; k < WORD_COUNTS; ++k) {
+              target = replacedWord(currentWord, j, A_CODE + k);
+              if (dictSet.has(target)) {
+                tmpPath = currentPath.slice();
+                tmpPath.push(target);
+                nextPaths.push(tmpPath);
+                currentSet.add(target);
+              }
+            }
+          }
+        }
+      }
+      // Put generated paths for next loop.
+      if (isReversing) {
+        endPath = nextPaths;
+      } else {
+        startPath = nextPaths;
+      }
+
+      if (startSet.size > endSet.size) {
+        targets = endSet.keys();
+        currentSet = startSet;
+      } else {
+        targets = startSet.keys();
+        currentSet = endSet;
+      }
+
+      for (target of targets) {
+        if (currentSet.has(target)) {
+          isConnected = true;
+          break;
+        }
+      }
+      if (isConnected) {
+        break;
+      } else {
+        // Use the set with smaller size.
+        isReversing = startPath.length > endPath.length ? true : false;
+        currentSet = isReversing ? endSet : startSet;
+        currentPaths = isReversing ? endPath : startPath;
+        currentLength = currentPaths.length;
+      }
+    }
+
+    // If break by connection, connect start with end now.
+    if (isConnected) {
+      currentLength = startPath.length;
+      pathLength = endPath.length;
+      // Reverse endPaths.
+      for (j = 0; j < pathLength; ++j) {
+        endPath[j].reverse();
+      }
+      for (i = 0; i < currentLength; ++i) {
+        currentPath = startPath[i];
+        currentWord = currentPath[currentPath.length - 1];
+        if (!endSet.has(currentWord)) {
+          continue;
+        }
+        for (j = 0; j < pathLength; ++j) {
+          target = endPath[j];
+          if (currentWord === target[0]) {
+            tmpPath = currentPath.concat(target.slice(1));
+            results.push(tmpPath);
+          }
+        }
+      }
+    }
+    return results;
+  };
+}
