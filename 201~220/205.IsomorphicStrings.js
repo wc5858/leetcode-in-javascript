775e1c44568d5ea46d3d291496a0509e5fff2c6d
ash <wc47501@gmail.com>
2018-08-02 13:24:41 +0800

    ans205

--- 201~220/205.IsomorphicStrings.js
+++ 201~220/205.IsomorphicStrings.js
@@ -4,15 +4,39 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-  let map = {}
+  let mapA = {}
+  let mapB = {}
+
   for (let i = 0; i < s.length; i++) {
-    if(map[s[i]]) {
-      if(t[i]!==map[s[i]]) {
+    if (mapA[s[i]]) {
+      if (t[i] !== mapA[s[i]]) {
         return false
       }
     } else {
-      map[s[i]] = t[i]
+      mapA[s[i]] = t[i]
+    }
+    if (mapB[t[i]]) {
+      if (s[i] !== mapB[t[i]]) {
+        return false
+      }
+    } else {
+      mapB[t[i]] = s[i]
     }
   }
   return true
-};
+}
+{
+  // 还有另一种优化方法：用index作关联
+  let isIsomorphic = function(s, t) {
+    // if (s.length != t.length) return false
+    // 题目已假定两串等长
+    const set1 = {}
+    const set2 = {}
+    for (let i = 0; i < s.length; i++) {
+      if (set1[s[i]] != set2[t[i]]) return false
+      set1[s[i]] = i
+      set2[t[i]] = i
+    }
+    return true
+  }
+}
